83da6c69b82855fae0bacadb4f2084a88b023531
Siddharth Chandra <chandrasiddharth975@gmail.com>
2026-02-27 19:21:28 +0530

    Lecture2

---
+++ lecture2/Operators.js
@@ -0,0 +1,31 @@
+var a=10;
+var b=20;
+var linebreak="<br>";
+
+document.write("a==b -> ");
+document.write(a == b);
+document.write(linebreak);
+
+document.write("a!=b -> ");
+document.write(a != b);
+document.write(linebreak);
+
+document.write("a > b -> ");
+document.write(a > b);
+document.write(linebreak);
+
+document.write("a < b -> ");
+document.write(a < b);
+document.write(linebreak);
+
+document.write("a>=b -> ");
+document.write(a >= b);
+document.write(linebreak);
+
+document.write("a<=b -> ");
+document.write(a <= b);
+document.write(linebreak);
+
+document.write("a===b -> ");
+document.write(a === b);
+document.write(linebreak);
---
+++ lecture2/loops.js
@@ -0,0 +1,32 @@
+// var count;
+// document.write("For Loop<br>");
+// for(count=0; count<5; count++){
+//     document.write("Count: " + count + "<br>");
+// }
+person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+for(key in person){
+    document.write(key + "<br>");
+}
+
+for (let x in person){
+    document.write("PERSON DETAILS: " + person[x] + "<br>");
+}
+// var count;
+// document.write("For Loop<br>");
+// for(count=0; count<5; count++){
+//     document.write("Count: " + count + "<br>");
+// }
+person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+for(key in person){
+    document.write(key + "<br>");
+}
